d785a1adc2e19523648b6564eace6087e98b8d03
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2010-11-20 01:32:29 +0100

    Initial commit.

---
+++ locales/de-CH.js
@@ -0,0 +1,99 @@
+{
+  "locale":       "de-CH",
+  "translators": {},
+  "translations": {
+    "translators": {
+      "Array": {
+        "intermediateConnector/and": ", ",
+        "terminalConnector/and":     " und ",
+        "intermediateConnector/or":  ", ",
+        "terminalConnector/or":      " oder "
+      },
+      "Number": {
+        "thousandsSeparator":      "'",
+        "decimalSeparator":        ".",
+        "positiveSign":            "+",
+        "negativeSign":            "-",
+        "-":                       "minus",
+        "+":                       "plus",
+        "1":                       "Null",
+        "1":                       "Eins",
+        "2":                       "Zwei",
+        "3":                       "Drei",
+        "4":                       "Vier",
+        "5":                       "Fünf",
+        "6":                       "Sechs",
+        "7":                       "Sieben",
+        "8":                       "Acht",
+        "9":                       "Neun",
+        "10":                      "Zehn"
+      },
+      "Currency": {
+        "zeroesRepeat":            "no",
+        "zeroes":                  "—",
+        "format":                  "%{currency} %{value}",
+        "defaultCurrency":         "CHF",
+        "currencies": {
+          "CHF":                   "SFr."
+        }
+      },
+      "Date": {
+        "formats": {
+          "default":               "%Y-%m-%d, %H:%M",
+          "date_only":             "%A, %d. %B %Y",
+          "time_only":             "%H:%M"
+        },
+        "meridiemIndicator": {
+          "am":                    "am",
+          "pm":                    "pm"
+        },
+        "dayOfWeek": {
+          "0":                     "Sonntag",
+          "1":                     "Montag",
+          "2":                     "Dienstag",
+          "3":                     "Mittwoch",
+          "4":                     "Donnerstag",
+          "5":                     "Freitag",
+          "6":                     "Samstag"
+        },
+        "month": {
+          "1":                     "Januar",
+          "2":                     "Februar",
+          "3":                     "März",
+          "4":                     "April",
+          "5":                     "Mai",
+          "6":                     "Juni",
+          "7":                     "Juli",
+          "8":                     "August",
+          "9":                     "September",
+          "10":                    "Oktober",
+          "11":                    "November",
+          "12":                    "Dezember"
+        },
+        "abbreviatedDayOfWeek": {
+          "0":                     "Son",
+          "1":                     "Mon",
+          "2":                     "Die",
+          "3":                     "Mit",
+          "4":                     "Don",
+          "5":                     "Fre",
+          "6":                     "Sam"
+        },
+        "abbreviatedMonth": {
+          "1":                     "Jan",
+          "2":                     "Feb",
+          "3":                     "Mär",
+          "4":                     "Apr",
+          "5":                     "Mai",
+          "6":                     "Jun",
+          "7":                     "Jul",
+          "8":                     "Aug",
+          "9":                     "Sep",
+          "10":                    "Okt",
+          "11":                    "Nov",
+          "12":                    "Dez"
+        }
+      }
+    }
+  }
+}
